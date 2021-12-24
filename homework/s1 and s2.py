1298cc617f5e5c890aefac0af9ae3d5c16350e94
ArnerZ <96292801+ArnerZ@users.noreply.github.com>
2021-12-24 12:10:14 +0300

    Add files via upload

---
+++ homework/s1 and s2.py
@@ -0,0 +1,10 @@
+s1 = "Hello"
+s2 = "Friend"
+print(len(s1))
+print(len(s2))
+print(s1[0])
+print(s2[-1])
+print(s1 == s2)
+print(s1 > s2)
+print('True' if s1 in s1 else 'False')
+print('False' if s2 in s1 else 'True')
